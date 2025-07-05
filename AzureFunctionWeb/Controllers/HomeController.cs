a6c11299e24384d7161e77059d42039c4c8319cc
maricel <maricelmb@gmail.com>
2025-07-05 18:16:34 +1000

    Call Azure function from web client

--- AzureFunctionWeb/Controllers/HomeController.cs
+++ AzureFunctionWeb/Controllers/HomeController.cs
@@ -7,15 +7,29 @@ namespace AzureFunctionWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        // http://localhost:7052/api/OnSalesUploadWriteToQueue
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SalesRequest salesRequest)
+        {
+            using var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("http://localhost:7052/api/");
+            await client.GetAsync("OnSalesUploadWriteToQueue");
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
--- JobAppFunction/OnSalesUploadWriteToQueue.cs
+++ JobAppFunction/OnSalesUploadWriteToQueue.cs
@@ -14,7 +14,7 @@ public class Function1
         _logger = logger;
     }
 
-    [Function("Function1")]
+    [Function("OnSalesUploadWriteToQueue")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
